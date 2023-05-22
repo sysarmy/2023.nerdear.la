cc0066339272c08360ae7388a795a041795cbace
Krapp Ramiro <krappramiro@disroot.org>
2023-05-22 20:06:04 -0300

    add placeholders for the sections, fix overflow in hero, remove unused index

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,24 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    user = {"username": "Miguel"}
-    return render_template("index.html", title="Home", user=user)
+    user = {"username": "this is a test :) "}
+    accordion_dataset = [
+        {
+            "number": 1,
+            "title": "Que es Nerdearla",
+            "description": "Nerdearla es lorem ipsum bla bla bla",
+        },
+        {
+            "number": 2,
+            "title": "Cuando se hace la Nerdearla",
+            "description": "La respuesta esta en tu corazon",
+        },
+        {
+            "number": 3,
+            "title": "Cúanto sale?",
+            "description": "Paga Jolo",
+        },
+    ]
+    return render_template(
+        "base.html", title="Home", accordion_dataset=accordion_dataset
+    )
