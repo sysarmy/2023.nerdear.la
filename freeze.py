ee74210f3614142a4a36880bd9fc41289fddc961
Krapp Ramiro <krappramiro@disroot.org>
2023-06-19 18:47:39 -0300

    fix frozen_flask build

--- freeze.py
+++ freeze.py
@@ -3,17 +3,32 @@
 from flask_frozen import Freezer
 from app import app
 
-freezer = Freezer(app)
+freezer = Freezer(app, log_url_for=False)
 
 
 @freezer.register_generator
-def en():
+def index():
     yield "/en/"
     yield "/es/"
 
 
+@freezer.register_generator
+def agenda():
+    yield "/en/agenda/"
+    yield "/es/agenda/"
+
+
+@freezer.register_generator
+def sponsors():
+    yield "/en/sponsors/"
+    yield "/es/sponsors/"
+
+
+@freezer.register_generator
+def code_of_conduct():
+    yield "/en/code_of_conduct/"  # Replace with the actual URL of the code of conduct endpoint
+    yield "/es/code_of_conduct/"  # Replace with the actual URL of the code of conduct endpoint
+
+
 if __name__ == "__main__":
-    try:
-        freezer.freeze()
-    except Exception as e:
-        print(f"{e}")
+    freezer.freeze()
