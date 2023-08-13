0e1abe0ae61c88915135df754d4a09e8b65ca1df
Krapp Ramiro <krappramiro@disroot.org>
2023-08-13 18:06:44 -0300

    fix static attempt 1

--- freeze.py
+++ freeze.py
@@ -6,7 +6,9 @@ from flask_babel import force_locale
 # Add base url for https://sysarmy.com/2023.nerdear.la/
 app.config["FREEZER_BASE_URL"] = "/2023.nerdear.la/"
 
-freezer = Freezer(app, log_url_for=False)
+freezer = Freezer(
+    app,
+)
 
 
 @freezer.register_generator
@@ -14,25 +16,25 @@ def redirect_index():
     yield url_for("redirect_index")
 
 
-@freezer.register_generator
-def index():
-    for lang in ["en", "es"]:
-        with force_locale(lang):
-            yield url_for("index", lang_code=lang)
+# @freezer.register_generator
+# def index():
+#     for lang in ["en", "es"]:
+#         with force_locale(lang):
+#             yield url_for("index", lang_code=lang)
 
 
-@freezer.register_generator
-def code_of_conduct():
-    for lang in ["en", "es"]:
-        with force_locale(lang):
-            yield url_for("code_of_conduct", lang_code=lang)
+# @freezer.register_generator
+# def code_of_conduct():
+#     for lang in ["en", "es"]:
+#         with force_locale(lang):
+#             yield url_for("code_of_conduct", lang_code=lang)
 
 
-@freezer.register_generator
-def sponsors():
-    for lang in ["en", "es"]:
-        with force_locale(lang):
-            yield url_for("sponsors", lang_code=lang)
+# @freezer.register_generator
+# def sponsors():
+#     for lang in ["en", "es"]:
+#         with force_locale(lang):
+#             yield url_for("sponsors", lang_code=lang)
 
 
 if __name__ == "__main__":
